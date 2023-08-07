7ef1531e9fe7d285ba0e41dc898adff893600df5
jotica0602 <alejandroeb5271@gmail.com>
2023-08-07 16:35:25 -0400

    variables can also be strings now

--- Lexer.cs
+++ Lexer.cs
@@ -12,54 +12,64 @@ using System.Xml.Schema;
 public class Lexer
 {
     #region Test Section
-    static void Main (string[] args)
-    {   
+    static void Main(string[] args)
+    {
         Console.Write("Write your arithmetic expression here>: ");
         string sourceCode = Console.ReadLine();
-
+        // try let a = 2^(3+2)^2, b = 3*4 in a*b;
         var Lexer = new Lexer(sourceCode);
 
-        List <Token> tokens = Lexer.Tokenize();
+        List<Token> tokens = Lexer.Tokenize();
         // Console.WriteLine(String.Join('\n', tokens));
-
         Parser parser = new(tokens);
-        
-        Console.WriteLine($"Result>: {parser.Parse()}");
-        
+        parser.Parse();
+
+        parser.variables.Clear();
     }
 
     #endregion
 
+    #region  Lexer Object
     private readonly string sourceCode;
     private int currentPosition;
 
-    public Lexer(string sourceCode){
+    public Lexer(string sourceCode)
+    {
         this.sourceCode = sourceCode;
         currentPosition = 0;
     }
 
+    #endregion 
+
+    #region Lexer Main Function: Tokenize
     // We need to split the Tokens
     // So I created a function named Tokenize, wich returns a List of Tokens containing all the Tokens
-    public List<Token> Tokenize(){
+    public List<Token> Tokenize()
+    {
 
         // Initialize List
         List<Token> tokens = new();
 
-        while (currentPosition < sourceCode.Length){
+        while (currentPosition < sourceCode.Length)
+        {
 
             // The character we are looking at is the current position of the sourceCode
             char currentChar = sourceCode[currentPosition];
 
             // If there's any white space we move on
-            if (char.IsWhiteSpace(currentChar)){
+            if (char.IsWhiteSpace(currentChar))
+            {
                 currentPosition++;
                 continue;
             }
             // Identifiers
-            if (IsLetter(currentChar))
+            else if (IsLetter(currentChar))
                 tokens.Add(IdKind());
+            // Quoted Strings
+            else if (currentChar == '"')
+                tokens.Add(StringKind());
             // Digits
-            else if(char.IsDigit(currentChar))
+            else if (char.IsDigit(currentChar))
                 tokens.Add(NumberKind());
             // Operators 
             else if (IsOperator(currentChar))
@@ -68,127 +78,152 @@ public class Lexer
             else if (IsPunctuator(currentChar))
                 tokens.Add(PuncutatorKind());
             // unknown tokens
-            else{
-                tokens.Add(new Token(TokenKind.Unknown,currentChar.ToString()));
+            else
+            {
+                tokens.Add(new Token(TokenKind.Unknown, currentChar.ToString()));
                 currentPosition++;
             }
         }
         return tokens;
     }
-   
-    # region TokenKind Adder Functions
 
-    private Token NumberKind(){
+    #endregion
+
+    #region TokenKind Adder Functions
+
+    private Token NumberKind()
+    {
 
         string number = "";
-        while((currentPosition < sourceCode.Length) && (char.IsDigit(sourceCode[currentPosition]) || char.Equals(sourceCode[currentPosition],','))){
+        while ((currentPosition < sourceCode.Length) && (char.IsDigit(sourceCode[currentPosition]) || sourceCode[currentPosition] == '.'))
+        {
             number += sourceCode[currentPosition];
             currentPosition++;
         }
-            // Token (Token Kind, Number Value)
+        number = number.Replace('.', ',');
+        // Token (Token Kind, Number Value)
         return new Token(TokenKind.Number, number);
     }
 
-    private Token IdKind(){
+    private Token IdKind()
+    {
 
         string idkind = "";
 
-        while (currentPosition < sourceCode.Length && (IsLetterOrDigit(sourceCode[currentPosition]) || sourceCode[currentPosition] == '_')){
-            idkind+=sourceCode[currentPosition];
+        while (currentPosition < sourceCode.Length && (IsLetterOrDigit(sourceCode[currentPosition]) || sourceCode[currentPosition] == '_'))
+        {
+            idkind += sourceCode[currentPosition];
             currentPosition++;
         }
-        if(IsKeyWord(idkind)){
+        if (IsKeyWord(idkind))
+        {
             return KeyWord(idkind);
         }
-        else 
+        else
             return new Token(TokenKind.Identifier, idkind);
     }
 
-    private Token OperatorKind(){
-        char _operator= sourceCode[currentPosition];
+    private Token StringKind()
+    {
+        currentPosition++;
+        string str = "";
+        while (currentPosition < sourceCode.Length && sourceCode[currentPosition] != '"')
+        {
+            str += sourceCode[currentPosition];
+            currentPosition++;
+        }
+        Next();
+        return new Token(TokenKind.String, str);
+    }
 
-        if(_operator == '+'){
+    private Token OperatorKind()
+    {
+        char _operator = sourceCode[currentPosition];
+
+        if (_operator == '+')
+        {
             Next();
             return new Token(TokenKind.PlusOperator, _operator.ToString());
         }
-        else if (_operator == '-'){           
+        else if (_operator == '-')
+        {
             Next();
             return new Token(TokenKind.MinusOperator, _operator.ToString());
         }
-        else if (_operator == '*'){
+        else if (_operator == '*')
+        {
             Next();
-            return new Token(TokenKind.MultOperator,_operator.ToString());
+            return new Token(TokenKind.MultOperator, _operator.ToString());
         }
-        else if (_operator == '/'){
+        else if (_operator == '/')
+        {
             Next();
-            return new Token(TokenKind.DivideOperator,_operator.ToString());
+            return new Token(TokenKind.DivideOperator, _operator.ToString());
         }
-        else if(_operator == '!'){
+        else if (_operator == '^')
+        {
             Next();
-            return new Token(TokenKind.NegationOperator,_operator.ToString());
+            return new Token(TokenKind.Power, _operator.ToString());
         }
-        else if (_operator == '^'){
+        else if (_operator == '@')
+        {
             Next();
-            return new Token(TokenKind.PowerOperator,_operator.ToString());
+            return new Token(TokenKind.Concat, _operator.ToString());
         }
-        else{
+        else
+        {
             Next();
-            return new Token(TokenKind.EqualsOperator,_operator.ToString());
+            return new Token(TokenKind.EqualsOperator, _operator.ToString());
         }
     }
 
-    private Token PuncutatorKind(){
-        char punctuator= sourceCode[currentPosition];
+    private Token PuncutatorKind()
+    {
+        char punctuator = sourceCode[currentPosition];
 
-        if( punctuator == '(' ){
+        if (punctuator == '(')
+        {
             Next();
             return new Token(TokenKind.LeftParenthesis, punctuator.ToString());
         }
-        else if ( punctuator == ')' ){           
+        else if (punctuator == ')')
+        {
             Next();
             return new Token(TokenKind.RightParenthesis, punctuator.ToString());
         }
-        else if (punctuator == '{'){
+        else if (punctuator == ',')
+        {
             Next();
-            return new Token(TokenKind.LeftCurlyBracket,punctuator.ToString());
+            return new Token(TokenKind.Comma, punctuator.ToString());
         }
-        else if (punctuator == '}'){
+        else if (punctuator == ';')
+        {
             Next();
-            return new Token(TokenKind.RightCurlyBracket,punctuator.ToString());
+            return new Token(TokenKind.Semicolon, punctuator.ToString());
         }
-        else if (punctuator == '['){
+        else if (punctuator == ':')
+        {
             Next();
-            return new Token(TokenKind.LeftBracket,punctuator.ToString());
+            return new Token(TokenKind.Colon, punctuator.ToString());
         }
-        else if (punctuator == ']'){
+        else if (punctuator == '"')
+        {
             Next();
-            return new Token(TokenKind.RightBracket,punctuator.ToString());
+            return new Token(TokenKind.Quote, punctuator.ToString());
         }
-        else if (punctuator == ','){
+        else
+        {
             Next();
-            return new Token(TokenKind.Comma,punctuator.ToString());
-        }
-        else if (punctuator == ';'){
-            Next();
-            return new Token(TokenKind.Semicolon,punctuator.ToString());
-        }
-        else if (punctuator== ';'){
-            Next();
-            return new Token(TokenKind.Colon,punctuator.ToString());
-        }
-        else if (punctuator == '"'){
-            Next();
-            return new Token(TokenKind.Quote,punctuator.ToString());
-        }
-        else{
-            Next();
-            return new Token(TokenKind.FullStop,punctuator.ToString());
+            return new Token(TokenKind.FullStop, punctuator.ToString());
         }
     }
 
-    private Token KeyWord(string idkind){
-        if(idkind == "let")
-            return new Token(TokenKind.letKeyWord,idkind);
+    private Token KeyWord(string idkind)
+    {
+        if (idkind == "let")
+            return new Token(TokenKind.letKeyWord, idkind);
+        else if (idkind == "function")
+            return new Token(TokenKind.function, idkind);
         else
             return new Token(TokenKind.inKeyWord, idkind);
     }
@@ -197,98 +232,124 @@ public class Lexer
 
     #region Utility Functions 
 
-    private void Next(){
+    private void Next()
+    {
         currentPosition++;
     }
 
-    private static bool IsLetter(char c){
+    private static bool IsLetter(char c)
+    {
         return char.IsLetter(c) || c == '_';
     }
 
-    private static bool IsLetterOrDigit(char c){
+    private static bool IsLetterOrDigit(char c)
+    {
         return char.IsLetterOrDigit(c) || c == '_';
     }
-    
-    private static bool IsOperator(char currentChar){
-        List<char> Operators  = new(){
-            '+', '-', '*', '/', '^', '=', '!'
+
+    private static bool IsOperator(char currentChar)
+    {
+        List<char> Operators = new(){
+            '+', '-', '*', '/', '^', '=', '!', '@',
+            '='
         };
 
-        foreach(var _operator in Operators)
-            if(currentChar==_operator)
+        foreach (var _operator in Operators)
+            if (currentChar == _operator)
                 return true;
-        
+
         return false;
     }
 
-    private static bool IsPunctuator(char currentChar){
-        List<char> Punctuators  = new(){
+    private static bool IsPunctuator(char currentChar)
+    {
+        List<char> Punctuators = new(){
             '(', ')', ';', ',', '{', '}',
             '[', ']','.','"',':'
         };
 
-        foreach(var puncutator in Punctuators)
-            if(currentChar==puncutator)
+        foreach (var puncutator in Punctuators)
+            if (currentChar == puncutator)
                 return true;
         return false;
     }
 
-    private static bool IsKeyWord(string idkind){
+    private static bool IsKeyWord(string idkind)
+    {
         List<string> keywords = new(){
-            "let",
-            "in"
+            "let", "function",  "else",
+            "in" , "if",        "true",
+            "false"
         };
-        foreach(var keyword in keywords)
-            if(idkind == keyword)
+        foreach (var keyword in keywords)
+            if (idkind == keyword)
                 return true;
         return false;
     }
-
-    
 
     #endregion
 }
 
+# region Token Objects
 // Token Object
-public class Token{
-    public TokenKind Kind { get; private set; }
-    public string Value { get; private set; }
-    public Token(TokenKind kind, string value){
+public class Token
+{
+    public TokenKind Kind { get; set; }
+    public string Value { get; set; }
+    public Token(TokenKind kind, string value)
+    {
         Kind = kind;
         Value = value;
     }
     // Show TokenKind and Value
-    public override string ToString(){
+    public override string ToString()
+    {
         return $"{Kind}: {Value}";
+    }
+}
+
+public class VarToken : Token
+{
+    public object VarValue { get; set; }
+    public VarToken(TokenKind kind, string value, object varValue) : base(kind, value)
+    {
+        VarValue = varValue;
+    }
+    public override string ToString()
+    {
+        return $"{Kind}Kind Name:{Value} VarValue: {VarValue}";
     }
 }
 
 // Token Kinds
 
-public enum TokenKind{
+public enum TokenKind
+{
     // Identifiers
     Identifier,
+    // Variable
+    Variable,
     // KeyWords
-    letKeyWord,inKeyWord,
-    
+    letKeyWord, inKeyWord, function,
+    // Strings
+    String,
+    // Number
     Number,
-
     //Operators
-    PlusOperator,   MinusOperator,  MultOperator, DivideOperator,
-    EqualsOperator, NegationOperator,  PowerOperator,
-
+    PlusOperator, MinusOperator, MultOperator, DivideOperator,
+    Equals, Negation, Power, Concat, EqualsOperator,
     // Punctuators
-    LeftParenthesis,    RightParenthesis,
-    LeftBracket,        RightBracket,
-    LeftCurlyBracket,   RightCurlyBracket,
+    LeftParenthesis, RightParenthesis,
+    LeftBracket, RightBracket,
+    LeftCurlyBracket, RightCurlyBracket,
     ////////////////////////////////////
-    Comma,  Colon,  Semicolon,  FullStop,
+    Comma, Colon, Semicolon, FullStop,
     Quote,
 
     EndOfFile,
     Unknown
 }
-
+#endregion
 
 
 
--- Parser.cs
+++ Parser.cs
@@ -1,106 +1,239 @@
 // After we made our list of Tokens, we need to find the sense of the expression, if it exists.
-public class Parser{
+public class Parser
+{
+
+    #region Parser Object
     // This is the List
     private List<Token> tokens;
     // Current Index
     private int currentTokenIndex;
     // Current Token
     private Token currentToken;
+    // VarList to keep record of created Variables
+    public List<VarToken> variables = new List<VarToken>();
+
     // Previous Token
-    public Parser(List<Token> tokens){
-        this.tokens=tokens;
-        currentTokenIndex=0;
-        currentToken=tokens[currentTokenIndex];
+    public Parser(List<Token> tokens)
+    {
+        this.tokens = tokens;
+        currentTokenIndex = 0;
+        currentToken = tokens[currentTokenIndex];
     }
 
     // Now we can move through our Tokens List
-    public void Next(){
+    public void Next()
+    {
         currentTokenIndex++;
-        if(currentTokenIndex<tokens.Count())
-            currentToken=tokens[currentTokenIndex];
+        if (currentTokenIndex < tokens.Count())
+            currentToken = tokens[currentTokenIndex];
         else
-            currentToken=new Token(TokenKind.EndOfFile,"");
+            currentToken = new Token(TokenKind.EndOfFile, "");
     }
 
-    private double ParseToken(){
+    #endregion
+
+    #region Parser Recursive Functions
+
+    private object ParseFactor()
+    {
         // if the token we are looking at is a number, we parse it, return it and move to the next
-        if(currentToken.Kind==TokenKind.Number){
-            double number = double.Parse(currentToken.Value);
+        if (currentToken.Kind == TokenKind.Number)
+        {
+            double factor = double.Parse(currentToken.Value);
             Next();
-            return number;
+            return factor;
         }
-        // if we find a left parenthesis we will keep parsing until we find a right parenthesis, if we don't find any
+
+        // same with strings
+        else if (currentToken.Kind == TokenKind.String)
+        {
+            string str = currentToken.Value;
+            Next();
+            return str;
+        }
+
+        // if there is an identifier, we check if it is a variable and return its value
+        else if (currentToken.Kind == TokenKind.Identifier)
+        {
+            object factor = null;
+            bool exists = false;
+            foreach (var variable in variables)
+            {
+                if (currentToken.Value == variable.Value)
+                {
+                    factor = variable.VarValue;
+                    exists = true;
+                    break;
+                }
+            }
+
+            Next();
+            if (exists == false) throw new Exception($"Variable does not exists: {tokens[currentTokenIndex]}");
+            else return factor;
+        }
+
+        // in keyword means that we evaluating an expression
+        else if (currentToken.Kind == TokenKind.inKeyWord)
+        {
+            Next();
+            object factor = ParseExpression();
+            return factor;
+        }
+
+        // let keyword means that we are declaring a variable
+        else if (currentToken.Kind == TokenKind.letKeyWord)
+        {
+            CreateVar(variables);
+
+            // after creating vars, we stepped into in keyword
+            // and need to parse the next expression
+
+            object factor = ParseExpression();
+
+            return factor;
+        }
+        // if we find a left parenthesis we will parse the next expression until we find a right parenthesis, if we don't find any
         // it will be a syntax error.
-        else if (currentToken.Kind==TokenKind.LeftParenthesis){
+        else if (currentToken.Kind == TokenKind.LeftParenthesis)
+        {
             Next();
-            double number = ParseSum();
-            if(currentToken.Kind!=TokenKind.RightParenthesis)
-                throw new InvalidOperationException ("Syntax Error");
-            Next();
-            return number;
-        }
-        // for negative arithmetic expressions
-        else if (currentToken.Kind==TokenKind.MinusOperator){
-            Next();
-            double number = 0-ParseToken();
-            return number;
-        }
-        else 
-            throw new InvalidOperationException ("Syntax Error");
-    }
-
-    private double ParseSum(){
-        double expressionResult = ParseMult(); 
-
-        while(currentToken.Kind==TokenKind.PlusOperator || currentToken.Kind==TokenKind.MinusOperator){
-            Token operatorToken = currentToken;
-            Next();
-            double nextToken = ParseMult();
-            if(operatorToken.Kind==TokenKind.PlusOperator)
-                expressionResult+=nextToken;
-            else if (operatorToken.Kind==TokenKind.MinusOperator)
-                expressionResult-=nextToken;
-        }
-        return expressionResult;
-    }
-    private double ParseMult(){
-        double expressionResult = ParsePow();
-        while(currentToken.Kind==TokenKind.MultOperator || currentToken.Kind==TokenKind.DivideOperator){
-            Token operatorToken = currentToken;
-            Next();
-            double nextToken = ParsePow();
-           if(operatorToken.Kind==TokenKind.MultOperator)
-                expressionResult*=nextToken;
-            else if(operatorToken.Kind==TokenKind.DivideOperator)
-                expressionResult/=nextToken;
-        }
-        return expressionResult;
-    }
-
-    private double ParsePow(){
-        double expressionResult = ParseToken();
-        while(currentToken.Kind==TokenKind.PowerOperator){
-            Token operatorToken = currentToken;
-            Next();
-            if(operatorToken.Kind!=TokenKind.LeftParenthesis)
-                expressionResult = Math.Pow(expressionResult,ParseSum());
-            else
+            object factor = ParseExpression();
+            if (currentToken.Kind != TokenKind.RightParenthesis)
                 throw new InvalidOperationException("Syntax Error");
+            Next();
+            return factor;
         }
-        return expressionResult;
 
-    }
+        // for negative arithmetic expressions
+        else if (currentToken.Kind == TokenKind.MinusOperator)
+        {
+            Next();
+            double factor = 0 - (double)ParseFactor();
+            return factor;
+        }
 
-    public double Parse(){
-        if(tokens.Count() == 0)
-            throw new Exception("There's nothing to parse");
-        double expressionResult = ParseSum();
-        if(currentToken.Kind!=TokenKind.EndOfFile)
+        else
             throw new InvalidOperationException("Syntax Error");
-        return expressionResult;
+    }
+
+    private object ParseExpression()
+    {
+        object expressionResult = ParseTerm();
+        // if the expression is a string we  will 
+        if (expressionResult is string)
+        {
+            while (currentToken.Kind == TokenKind.Concat)
+            {
+                Next();
+                string nextToken = (string)ParseTerm();
+                expressionResult += nextToken;
+            }
+            return expressionResult;
+        }
+        else
+        {
+            double _expressionResult = (double)expressionResult;
+            while (currentToken.Kind == TokenKind.PlusOperator || currentToken.Kind == TokenKind.MinusOperator)
+            {
+                Token operatorToken = currentToken;
+                Next();
+                double nextToken = (double)ParseTerm();
+                if (operatorToken.Kind == TokenKind.PlusOperator)
+                    _expressionResult += nextToken;
+                else if (operatorToken.Kind == TokenKind.MinusOperator)
+                    _expressionResult -= nextToken;
+            }
+            return _expressionResult;
+        }
+    }
+    private object ParseTerm()
+    {
+        object term = _ParseTerm();
+        if (term is string)
+            return term;
+        else
+        {
+            double _term = (double)term;
+            while (currentToken.Kind == TokenKind.MultOperator || currentToken.Kind == TokenKind.DivideOperator)
+            {
+                Token operatorToken = currentToken;
+                Next();
+
+                double nextToken = (double)_ParseTerm();
+
+                if (operatorToken.Kind == TokenKind.MultOperator)
+                    _term *= nextToken;
+
+                else if (operatorToken.Kind == TokenKind.DivideOperator)
+                    _term /= nextToken;
+            }
+            return _term;
+        }
+    }
+
+    private object _ParseTerm()
+    {
+        object term = ParseFactor();
+        if (term is string)
+            return term;
+        else
+        {
+            double _term = (double)term;
+            while (currentToken.Kind == TokenKind.Power)
+            {
+                Token operatorToken = currentToken;
+                Next();
+                if (operatorToken.Kind != TokenKind.LeftParenthesis)
+                    _term = Math.Pow((double)_term, (double)ParseExpression());
+                else
+                    throw new InvalidOperationException("Syntax Error");
+            }
+            return _term;
+        }
+    }
+
+    #endregion
+
+    #region Parser Main Function
+    public void Parse()
+    {
+        if (tokens.Count() == 0)
+        {
+            variables.Clear();
+            throw new Exception("There's nothing to parse");
+        }
+        Console.WriteLine(ParseExpression());
+
+        if (currentToken.Kind == TokenKind.EndOfFile)
+        {
+            variables.Clear();
+            throw new InvalidOperationException($"Syntax Error: ; is missing after {currentToken.Value}");
+        }
+    }
+
+    #endregion
+
+    // Create Variable Utility Function
+    private void CreateVar(List<VarToken> variables)
+    {
+        Next();
+        if (currentToken.Kind != TokenKind.Identifier)
+            throw new Exception($"Syntax Error: you have not defined an identifier after {currentToken.Value}");
+        int vartokenindex = currentTokenIndex;
+
+        Next();
+        if (currentToken.Kind != TokenKind.EqualsOperator)
+            throw new Exception($"Syntax Error: = is missing after {tokens[currentTokenIndex - 1]}");
+
+        Next();
+        VarToken variable = new VarToken(TokenKind.Variable, tokens[vartokenindex].Value, ParseExpression());
+        // tokens[vartokenindex]=new VarToken(TokenKind.Variable,tokens[vartokenindex].Value,ParseExpression());
+        variables.Add(variable);
+
+        if (currentToken.Kind == TokenKind.Comma)
+            CreateVar(variables);
+        if (currentToken.Kind != TokenKind.inKeyWord)
+            throw new Exception($"Syntax Error: in is missing after {tokens[currentTokenIndex - 1]}");
     }
 }
 
-public class Variable{
-    
-}
