c2e72e7a4bd0ce76c1e715ea8161f06dbe05f678
jotica0602 <alejandroeb5271@gmail.com>
2023-10-12 23:21:00 -0400

    minor changes

--- App/Program.cs
+++ App/Program.cs
@@ -5,84 +5,44 @@ namespace Interpreter
     {
         static void Main()
         {
-            // Auto();
-            Manual();
-            static void Auto()
+            Run();
+        }
+
+        public static void Run()
+        {
+            Console.Clear();
+            while (true)
             {
-                int i = 0;
-                while (true)
+                try
                 {
-                    try
+                    Console.Write(">");
+                    string sourceCode = Console.ReadLine()!;
+                    var Lexer = new Lexer(sourceCode);
+
+
+                    if (Lexer.sourceCode == string.Empty)
                     {
-                        // Automatic tests
-                        string[] strings = { "function fib(n) => if(n>=1) fib(n-1) + fib(n-2) else 1;","print(fib(5));" };
-                        var Lexer = new Lexer(strings[i]);
-
-                        if (Lexer.sourceCode == string.Empty)
-                        {
-                            Diagnostics.Errors.Add("Empty Entry");
-                            throw new Exception();
-                        }
-
-                        List<Token> tokens = Lexer.Tokenize();
-                        // Console.WriteLine(string.Join('\n',tokens));
-
-                        Parser parser = new Parser(tokens, new List<Dictionary<string,object>>());
-                        parser.Parse();
-                        parser.ClearVariables();
-
-                        i++;
-
-                        if (i >= strings.Length)
-                        {
-                            break;
-                        }
+                        Diagnostics.Errors.Add("Empty Entry");
+                        throw new Exception();
                     }
 
-                    catch (Exception)
-                    {
-                        Console.WriteLine(Diagnostics.Errors[0]);
-                        Diagnostics.Errors.Clear();
-                        break;
-                    }
+                    List<Token> tokens = Lexer.Tokenize();
+
+
+                    Parser parser = new Parser(tokens, new List<Dictionary<string, object>>());
+                    parser.Parse();
+                    parser.ClearVariables();
+
                 }
-            }
-
-            void Manual()
-            {
-                Console.Clear();
-                while (true)
+                catch (Exception)
                 {
-                    try
-                    {
-                        Console.Write(">");
-                        string sourceCode = Console.ReadLine()!;
-                        var Lexer = new Lexer(sourceCode);
-
-
-                        if (Lexer.sourceCode == string.Empty)
-                        {
-                            Diagnostics.Errors.Add("Empty Entry");
-                            throw new Exception();
-                        }
-
-                        List<Token> tokens = Lexer.Tokenize();
-
-
-                        Parser parser = new Parser(tokens,new List<Dictionary<string,object>>());
-                        parser.Parse();
-                        parser.ClearVariables();
-
-                    }
-                    catch (Exception)
-                    {
-                        Console.WriteLine(Diagnostics.Errors[0]);
-                        Diagnostics.Errors.Clear();
-                        continue;
-                    }
+                    Console.WriteLine(Diagnostics.Errors[0]);
+                    Diagnostics.Errors.Clear();
+                    continue;
                 }
             }
         }
     }
-
 }
+
+
